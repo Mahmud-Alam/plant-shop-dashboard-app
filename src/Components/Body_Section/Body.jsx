bd776ec42865e7511adc20d5763ec3ba58bf7594
mahmud-alam <official.mahmud23@gmail.com>
2023-09-15 16:44:22 +0600

    setup sidebar jsx and scss

--- src/Components/Body_Section/Body.jsx
+++ src/Components/Body_Section/Body.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Body.css'
+import Top from './Top_Section/Top'
+import Listing from './Listing_Section/Listing'
+import Activity from './Activity_Section/Activity'
 
 const Body = () => {
   return (
-    <div>Body</div>
+    <div className="mainContent">
+      <Top/>
+
+      <div className="bottom flex">
+        <Listing/>
+        <Activity/>
+      </div>
+    </div>
   )
 }
 
