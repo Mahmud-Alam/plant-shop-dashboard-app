31df5665249c55fa6c933290d33bbfdc32e68a69
mahmud-alam <official.mahmud23@gmail.com>
2023-09-16 09:05:34 +0600

    [ADD] Recent Activity section

--- src/Components/Body_Section/Activity_Section/Activity.jsx
+++ src/Components/Body_Section/Activity_Section/Activity.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
 import './Activity.css'
+import { BsArrowRightShort } from "react-icons/bs";
+import user from "../../../Assets/mahmud-alam.jpg";
+import user1 from "../../../Assets/user1.jpg";
+import user2 from "../../../Assets/user2.jpg";
+import user3 from "../../../Assets/user3.jpg";
+
 
 const Activity = () => {
   return (
-    <div>Activity</div>
+    <div className='activitySection'>
+      <div className="heading flex">
+        <h1>Recent Activity</h1>
+        <button className="btn flex">
+          See All
+          <BsArrowRightShort className='icon'/>
+        </button>
+      </div>
+
+      <div className="secContainer grid">
+        <div className="singleCustomer flex">
+          <img src={user} alt="Customer Image" />
+          <div className="customerDetails">
+            <span className="name">Mahmud Alam</span>
+            <small>Ordered a new plant</small>
+          </div>
+          <div className="duration">
+            2 min ago
+          </div>
+        </div>
+
+        <div className="singleCustomer flex">
+          <img src={user1} alt="Customer Image" />
+          <div className="customerDetails">
+            <span className="name">Dale Carnegie</span>
+            <small>Ordered a new plant</small>
+          </div>
+          <div className="duration">
+            2 min ago
+          </div>
+        </div>
+
+        <div className="singleCustomer flex">
+          <img src={user2} alt="Customer Image" />
+          <div className="customerDetails">
+            <span className="name">Vint Cerf</span>
+            <small>Ordered a new plant</small>
+          </div>
+          <div className="duration">
+            2 min ago
+          </div>
+        </div>
+
+        <div className="singleCustomer flex">
+          <img src={user3} alt="Customer Image" />
+          <div className="customerDetails">
+            <span className="name">Robert Downey Jr.</span>
+            <small>Ordered a new plant</small>
+          </div>
+          <div className="duration">
+            2 min ago
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
